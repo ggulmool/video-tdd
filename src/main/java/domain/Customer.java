7710c63599e46ca819c7c01c630d8c627cb1468f
ggulmool <kimny86@gmail.com>
2018-03-29 15:50:28 +0900

    customer(1) -> rental(N)

--- src/main/java/domain/Customer.java
+++ src/main/java/domain/Customer.java
@@ -1,17 +1,23 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
 
     private String name;
     private int point;
+    private List<Rental> rentals;
 
     public Customer(String name) {
         this.name = name;
+        this.rentals = new ArrayList<>();
     }
 
     public void rent(Videos videos, int rentalPeriod) {
         Rental rental = videos.rent(rentalPeriod);
         this.point += rental.getTotalPoint();
+        this.rentals.add(rental);
     }
 
     public int getPoint() {
